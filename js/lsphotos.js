d6730521dc58023fa696fbc2f19a36d55503a2de
James Judd <JJudd@norvax.net>
2016-09-26 16:41:17 -0500

    initial commit

---
+++ js/lsphotos.js
@@ -0,0 +1,99 @@
+// courtesy of http://stackoverflow.com/a/6274381/648350
+function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+// courtesy of http://stackoverflow.com/a/6700/648350
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
+(function() {
+  var jsonData = "../lsphotos/lsphotos.json";
+  $.getJSON( jsonData)
+    .done(function( data ) {
+        var numberOfResults = Object.size(data.images);
+        console.log(numberOfResults)
+        data.images = shuffle(data.images)
+      $.each( data.images, function( i, item ) {
+          $('<div class="col-sm-6 col-md-3 col-lg-3 web"><div class="portfolio-item"><div class="hover-bg"><a href="' + item.media_file_path + '" title="' + item.caption + '" rel="prettyPhoto"><div class="hover-text"><h4></h4><small></small> </div><img src="'+ item.small_media_file_path + '" class="img-responsive" alt=""> </a></div></div></div>').appendTo('.portfolio-items')
+
+        if ( i === 7 ) {
+          return false;
+        }
+      });
+    });
+})();
+
+
+// $(document).ready(function() {
+//     $.getJSON('../lsphotos/lsphotos.json', function(data) { //Ajax call
+//             console.log('loaded json')
+//             var item = data.images[Math.floor(Math.random() * data.images.length)]; //select image at random
+//             //build the complete <img> tag
+//             $('<div class="container">').appendTo('portfolio')
+//             $('<div class="section-title text-center center">').appendTo('#portfolio')
+//             $('<h2>Photos</h2>').appendTo('#portfolio')
+//             $('<hr>').appendTo('#portfolio')
+//             $('</div>').appendTo('#portfolio')
+//             $('<div class="row">').appendTo('#portfolio')
+//             $('<div class="portfolio-items">').appendTo('#portfolio')
+//             $('<div class="col-sm-6 col-md-3 col-lg-3 web">').appendTo('#portfolio')
+//             $('<div class="portfolio-item">').appendTo('#portfolio')
+//             $('<div class="hover-bg">').appendTo('#portfolio')
+//             $('<a href="' + item.media_file_path + '" title="' + item.caption + '" rel="prettyPhoto">').appendTo('#portfolio')
+//             $('<div class="hover-text">').appendTo('#portfolio')
+//             $('<img src="' + item.media_file_path + '" class="img-responsive" alt=""> </a>').appendTo('#portfolio')
+//             $('</div></div>           </div>       </div>').appendTo('#portfolio')
+//                 //final divs
+//             $('</div>           </div>       </div>').appendTo('#portfolio')
+//
+//         }
+//         .error(console.log('uh oh!'))
+//     );
+// });
+/*
+var jsonData = 'lsphotos.json';
+var keyArray = Object.keys(jsonData);
+
+function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+keyArray = shuffle(keyArray); // shuffle it!
+
+function showImage() {
+    for (var i = 0; i < 8; ++i) {
+        var current = data[keyArray[i]];
+        document.write('<a href="' + current.media_file_path +
+        '" title=" + jsondata.photos[random1].caption " rel="prettyPhoto"><div class="hover-text"><h4></h4><small.</small> </div><img src="' +
+        current.media_file_path + '" class="img-responsive" alt="Project Title"> </a>');
+    }
+}
+*/
+
+/*
+function showImage() {
+    document.write('<a href="' + jsonData.photos[random1].media_file_path +
+    '" title=" + jsondata.photos[random1].caption " rel="prettyPhoto"><div class="hover-text"><h4></h4><small.</small> </div><img src="' +
+    jsonData.photos[random1].media_file_path + '" class="img-responsive" alt="Project Title"> </a>');
+}
+*/
+// http://stackoverflow.com/questions/9991805/javascript-how-to-parse-json-array
+//
+//
+//<div class="col-sm-6 col-md-3 col-lg-3 web">
+//    <div class="portfolio-item">
+//        <div class="hover-bg">
+//            <a href="img/portfolio/03-large.jpg" title="Project description" rel="prettyPhoto">
+//                <div class="hover-text">
+//                    <h4>Project Title</h4>
+//                    <small>Web Design</small> </div>
+//                <img src="img/portfolio/03-small.jpg" class="img-responsive" alt="Project Title"> </a>
+//        </div>
+//    </div>
+//</div>
---
+++ python/instagram.py
@@ -0,0 +1,182 @@
+import requests
+import re
+import json
+import os.path
+import time
+import random
+import logging
+import sys
+from pprint import pprint
+import pickle
+from PIL import Image
+
+
+def get_numbers_from_filename(filename):
+    return re.search(r'\d+', filename).group(0)
+
+logger = logging.getLogger()
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+
+lsphotos_json = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lsphotos', 'lsphotos.json'))
+media_file_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lsphotos'))
+
+insta_url = 'https://www.instagram.com/explore/tags/'
+
+tags = [
+    'lovestarbicyclebags',
+    'lovestarraceclub',
+    'lovestarfactoryteam'
+]
+
+tag_page = {}
+
+
+def merge_two_dicts(x, y):
+    '''Given two dicts, merge them into a new dict as a shallow copy.'''
+    z = x.copy()
+    z.update(y)
+    return z
+
+
+def resize_big_images(image_path):
+    img = Image.open(image_path)
+    max_size = 800
+    original_size = max(img.size[0], img.size[1])
+    if original_size >= max_size:
+        if (img.size[0] < img.size[1]):
+            resized_width = max_size
+            resized_height = int(round((max_size / float(img.size[0])) * img.size[1]))
+        else:
+            resized_height = max_size
+            resized_width = int(round((max_size / float(img.size[1])) * img.size[0]))
+        img = img.resize((resized_width, resized_height), Image.ANTIALIAS)
+        img.save(image_path, 'JPEG')
+        logger.info('image resized')
+
+
+def create_smaller_images(image_path):
+    fname, file_extension = os.path.splitext(image_path)
+    img = Image.open(image_path)
+    width, height = img.size
+    left = (width - 800) / 2
+    right = (width + 800) / 2
+    top = (height - 800) / 2
+    bottom = (height + 800) / 2
+    new_img = img.crop((left, top, right, bottom))
+    new_img = new_img.resize((400, 400), Image.ANTIALIAS)
+    new_img.save(fname + '_small' + file_extension)
+    logger.info('thumbnail created')
+
+
+def parse_json(tag_page_json):
+    for item, entry in enumerate(tag_page_json):
+        dir_list = os.listdir(media_file_folder)
+        max_list = []
+        for item in dir_list:
+            if item[:5] == 'image':
+                max_list.append(item)
+        if not max_list:
+            n = 1
+        else:
+            max_item = max(max_list)
+            n = int(get_numbers_from_filename(max_item))
+            n += 1
+        fej = open(os.path.join(media_file_folder, 'downloaded_photos.pkl'), 'rb')
+        try:
+            downloaded_photos = pickle.load(fej)
+        except EOFError:
+            downloaded_photos = []
+        media_id = entry['id']
+        media_url = entry['display_src']
+        media_caption = entry['caption']
+        media_file_name = 'image' + '%0.6d' % n + '.jpg'
+        small_media_file_name = 'image' + '%0.6d' % n + '_small.jpg'
+        media_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lsphotos', media_file_name))
+        small_media_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lsphotos', small_media_file_name))
+        if entry['is_video'] is False and media_id not in downloaded_photos:
+            with open(media_file_path, 'wb') as handle:
+                response = requests.get(media_url, stream=True)
+                if not response.ok:
+                    print 'couldn\'t download file: ' + media_id
+                for block in response.iter_content(1024):
+                    handle.write(block)
+            entry = {}
+            media_index = n - 1
+            entry['media_id'] = media_id
+            entry['media_url'] = media_url
+            entry['caption'] = media_caption
+            entry['media_file_path'] = os.path.relpath(
+                media_file_path, os.path.join(os.path.dirname(__file__), '..'))
+            entry['small_media_file_path'] = os.path.relpath(
+                small_media_file_path, os.path.join(os.path.dirname(__file__), '..'))
+            f = open(lsphotos_json, 'r')
+            lsphotos_dict = json.loads(f.read())
+            f.close()
+            lsphotos_dict['images'].insert(media_index, entry)
+            with open(lsphotos_json, 'w') as fp:
+                json.dump(lsphotos_dict, fp)
+            downloaded_photos.append(media_id)
+            with open(os.path.join(media_file_folder, 'downloaded_photos.pkl'), 'wb') as outfile:
+                pickle.dump(downloaded_photos, outfile)
+            logging.info('photo added ' + media_id)
+            resize_big_images(media_file_path)
+            create_smaller_images(media_file_path)
+            time.sleep(random.randint(1, 10))
+
+
+def get_json(url, tag):
+    # new_url = insta_url + tag
+    r = requests.get(url)
+    text = r.text
+    insta_json = json.loads(re.search(r"window._sharedData\s*=\s*(.*);", text).group(1))
+    top_posts = insta_json['entry_data']['TagPage'][0]['tag']['top_posts']['nodes']
+    parse_json(top_posts)
+    tag_page = insta_json['entry_data']['TagPage'][0]['tag']['media']['nodes']
+    parse_json(tag_page)
+    while insta_json['entry_data']['TagPage'][0]['tag']['media']['page_info']['has_next_page'] is True:
+        cursor = insta_json['entry_data']['TagPage'][0]['tag']['media']['page_info']['end_cursor']
+        new_url = insta_url + tag + '/?max_id=' + cursor
+        logging.info('new_url: ' + new_url)
+        return new_url
+
+
+def rename_files():
+    dir_list = os.listdir(media_file_folder)
+    max_list = []
+    for item in dir_list:
+        if item[:5] == 'image':
+            max_list.append(item)
+    if not max_list:
+        n = 1
+    else:
+        max_item = max(max_list)
+        n = int(get_numbers_from_filename(max_item))
+        n += 1
+    for filename in os.listdir(media_file_folder):
+        fname, file_extension = os.path.splitext(filename)
+        if file_extension == '.jpg' and fname[:5] != 'image':
+            file_name = 'image' + '%0.6d' % n + file_extension
+            os.rename(os.path.join(media_file_folder, filename), os.path.join(media_file_folder, file_name))
+            ls_json = open(lsphotos_json)
+            ls_json = json.loads(ls_json.read())
+            ls_json[fname]['java_array_id'] = n - 1
+            ls_json[fname]['media_file_path'] = os.path.relpath(
+                os.path.join(media_file_folder, file_name), os.path.join(os.path.dirname(__file__), '..'))
+            with open(lsphotos_json, 'w') as fp:
+                json.dump(ls_json, fp)
+            n += 1
+            logging.info('photo renamed ' + fname + ' ' + file_name)
+
+
+for item in tags:
+    tagged_url = insta_url + item
+    while tagged_url:
+        tagged_url = get_json(tagged_url, item)
+        time.sleep(random.randint(1, 10))
