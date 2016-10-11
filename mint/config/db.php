aeda6f3006e94d8dace85f33411a74699a908597
James Judd <JJudd@norvax.net>
2016-10-11 14:40:31 -0500

    adding mint analytics

---
+++ mint/config/db.php
@@ -0,0 +1,22 @@
+<?php
+/******************************************************************************
+ Mint
+
+ Copyright 2004-2011 Shaun Inman. This code cannot be redistributed without
+ permission from http://www.shauninman.com/
+
+ More info at: http://www.haveamint.com/
+
+ ******************************************************************************
+ Configuration
+ ******************************************************************************/
+ if (!defined('MINT')) { header('Location:/'); } // Prevent viewing this file
+
+$Mint = new Mint (array
+(
+	'server'	=> 'm7wltxurw8d2n21q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306',
+	'username'	=> 'ygxqs4wkpiua616g',
+	'password'	=> 'mbjhs3rjfc2rkqx9',
+	'database'	=> 'cx5tsqa0qdlu54hc',
+	'tblPrefix'	=> 'mint_'
+));
